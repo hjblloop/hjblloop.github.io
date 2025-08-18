ac4d112a3633375b1360dc8ff1146f2ae9dcc2d0
hjblloop <jonathanc.jcho456852@gmail.com>
2025-08-18 15:52:24 -0400

    added output export fro compiled ts files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 //change to '' when running build locally. switch to github when deploying to prod
 const nextConfig: NextConfig = {
+    output: 'export',
     basePath: '/hjblloop.github.io',
     assetPrefix: '/hjblloop.github.io/',
 };
